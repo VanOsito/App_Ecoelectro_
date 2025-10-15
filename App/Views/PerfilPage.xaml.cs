b4692b7737ad1398919d0ed637e1f9e31cc8c22c
Vania <van.troncosom@duocuc.cl>
2025-10-15 00:02:21 -0300

    add: perfil usuario

--- App/Views/PerfilPage.xaml.cs
+++ App/Views/PerfilPage.xaml.cs
@@ -1,21 +1,50 @@
-using static App.Models.RegionChile;
 using App.Data;
+using App.Models;
 using Microsoft.Maui.Controls;  
 using System;
-using App.Models;
+using static App.Models.RegionChile;
 namespace App.Views;
 
 public partial class PerfilPage : ContentPage
 {
-	public PerfilPage()
+    private readonly DatabaseService _dbService = new DatabaseService();
+    private Usuario _usuario;
+    public PerfilPage()
 	{
 		InitializeComponent();
+        if (App.UsuarioActual != null)
+        {
+            CargarDatosUsuario();
+        }
 
         if (App.UsuarioActual == "admin@admin.com")
             btnGestionUsuarios.IsVisible = true;
         else
             btnGestionUsuarios.IsVisible = false;
+
+
     }
+    private async void CargarDatosUsuario()
+    {
+        string correo = App.UsuarioActual;
+
+        if (string.IsNullOrEmpty(correo))
+            return;
+
+        var usuarios = await _dbService.ObtenerUsuarios();
+        var usuario = usuarios.FirstOrDefault(u => u.Correo == correo);
+
+        if (usuario != null)
+        {
+            lblBienvenida.Text = $"Bienvenido, {usuario.Nombre}";
+            lblNombre.Text = $"Nombre: {usuario.Nombre}";
+            lblCorreo.Text = $"Correo: {usuario.Correo}";
+            lblRegion.Text = $"Región: {usuario.RegionUsuario}";
+            lblComuna.Text = $"Comuna: {usuario.ComunaUsuario}";
+        }
+    }
+
+
     private async void cerrar(object sender, EventArgs e)
     {
         bool confirmar = await DisplayAlert("Cerrar sesión", "¿Deseas cerrar sesión?", "Sí", "No");
